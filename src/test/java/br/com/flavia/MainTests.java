723320a9d7353e48fdbc719e2a870b4afbff619a
Flávia <flabenik@gmail.com>
2024-05-28 23:51:37 -0300

    first commit

---
+++ src/test/java/br/com/flavia/MainTests.java
@@ -0,0 +1,13 @@
+package br.com.flavia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
